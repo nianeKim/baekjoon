983b6906cb8626d5be84f09cd34601e981dba675
nianeKim <84488855+nianeKim@users.noreply.github.com>
2021-09-20 21:24:00 +0900

    no.2675

---
+++ src/baekjoon/RepeatString.java
@@ -0,0 +1,26 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class RepeatString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        int r=0;
+        String s = "";
+        for(int i=1; i<=t; i++) {
+            r = sc.nextInt();
+            s = sc.next();
+
+            String[] st = new String[20];
+            st = s.split("");
+            for(int j=0; j<st.length; j++) {
+                for(int k=0; k<r; k++) {
+                    System.out.print(st[j]);
+                }
+            }
+            System.out.println("");
+        }
+        sc.close();
+    }
+}
