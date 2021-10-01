2673895891269b3dda7cf1fc1a4f53ff50809b6d
nianeKim <84488855+nianeKim@users.noreply.github.com>
2021-10-02 00:37:40 +0900

    no.2908

---
+++ src/baekjoon/Sangsu.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Sangsu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        a = (a%10)*100 + (a/10%10)*10 + (a/100%10);
+        b = (b%10)*100 + (b/10%10)*10 + (b/100%10);
+
+        int c = (a>b) ? a : b;
+
+        System.out.println(c);
+        sc.close();
+    }
+}
